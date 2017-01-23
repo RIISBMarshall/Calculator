05e989e3e60a6f0fe1b864aed69c9bc2b9b6c666
Brian Marshall <bmarshall@RIIS.com>
2017-01-23 16:20:03 -0500

    Added decimal functionality

--- app/src/main/java/com/example/bmarshall/calculator/MainActivity.java
+++ app/src/main/java/com/example/bmarshall/calculator/MainActivity.java
@@ -7,15 +7,24 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    //need to handle only decimal entered exception
+    //need to add color theme choosing
+
+    private NumberFormat nf;
 
     private TextView screen;
 
     String numberAsText;
 
-    private int savedNumber;
+    private double savedNumber;
 
     private boolean modeWasLast;
+    private boolean numberHasDecimal;
 
     private Mode currentMode;
 
@@ -27,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nf = new DecimalFormat("##.###");
         screen = (TextView) findViewById(R.id.screenTextView);
         numberAsText = "0";
         screen.setText(numberAsText);
@@ -42,9 +52,16 @@ public class MainActivity extends AppCompatActivity {
         if (numberAsText.equals("0")) {
             numberAsText = "";
         }
-        numberAsText = numberAsText + numberButton.getText();
-        screen.setText(numberAsText);
-        modeWasLast = false;
+        if(numberHasDecimal && numberButton.getText().equals(".")){
+            //do nothing
+        } else {
+            numberAsText = numberAsText + numberButton.getText();
+            screen.setText(numberAsText);
+            modeWasLast = false;
+        }
+        if(numberButton.getText().equals(".")){
+            numberHasDecimal = true;
+        }
     }
 
     public void onClickModeButton(View view) {
@@ -68,33 +85,35 @@ public class MainActivity extends AppCompatActivity {
                 screen.setText("fail");
         }
         modeWasLast = true;
-        savedNumber = Integer.parseInt(numberAsText);
+        savedNumber = Double.parseDouble(numberAsText);
+        numberHasDecimal = false;
     }
 
     public void onClickEquals(View view) {
         switch (currentMode) {
             case ADD:
-                savedNumber = savedNumber + Integer.parseInt(numberAsText);
+                savedNumber = savedNumber + Double.parseDouble(numberAsText);
                 break;
             case SUBTRACT:
-                savedNumber = savedNumber - Integer.parseInt(numberAsText);
+                savedNumber = savedNumber - Double.parseDouble(numberAsText);
                 break;
             case DIVIDE:
                 if (numberAsText.equals("0")) {
                     Toast toast = Toast.makeText(this, "Can't divide by Zero!", Toast.LENGTH_SHORT);
                     toast.show();
                 } else {
-                savedNumber = savedNumber / Integer.parseInt(numberAsText);
+                savedNumber = savedNumber / Double.parseDouble(numberAsText);
                 }
                 break;
             case MULTIPLY:
-                savedNumber = savedNumber * Integer.parseInt(numberAsText);
+                savedNumber = savedNumber * Double.parseDouble(numberAsText);
                 break;
             default:
                 //do nothing
         }
-        numberAsText = Integer.toString(savedNumber);
+        numberAsText = nf.format(savedNumber);
         screen.setText(numberAsText);
+        numberHasDecimal = false;
 
     }
 
@@ -102,5 +121,6 @@ public class MainActivity extends AppCompatActivity {
         savedNumber = 0;
         numberAsText = "0";
         screen.setText(numberAsText);
+        numberHasDecimal = false;
     }
 }
