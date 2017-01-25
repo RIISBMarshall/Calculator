2fa13769eeef700c3ef04a624075a37b2f372b54
Brian Marshall <bmarshall@RIIS.com>
2017-01-25 16:53:04 -0500

    added color functionality

--- app/src/main/java/com/example/bmarshall/calculator/MainActivity.java
+++ app/src/main/java/com/example/bmarshall/calculator/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.bmarshall.calculator;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,13 +15,16 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     //need to handle only decimal entered exception
-    //need to add color theme choosing
 
     private NumberFormat nf;
 
+    GridLayout background;
+
     private TextView screen;
 
-    String numberAsText;
+    private String numberAsText;
+
+    private int color;
 
     private double savedNumber;
 
@@ -38,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nf = new DecimalFormat("##.###");
         screen = (TextView) findViewById(R.id.screenTextView);
-        numberAsText = "0";
+        background = (GridLayout) findViewById(R.id.activity_main);
+        numberAsText = getString(R.string.zero);
         screen.setText(numberAsText);
         modeWasLast = false;
         savedNumber = 0;
@@ -49,17 +55,17 @@ public class MainActivity extends AppCompatActivity {
         if (modeWasLast) {
             numberAsText = "";
         }
-        if (numberAsText.equals("0")) {
+        if (numberAsText.equals(getString(R.string.zero))) {
             numberAsText = "";
         }
-        if(numberHasDecimal && numberButton.getText().equals(".")){
+        if (numberHasDecimal && numberButton.getText().equals(".")) {
             //do nothing
         } else {
             numberAsText = numberAsText + numberButton.getText();
             screen.setText(numberAsText);
             modeWasLast = false;
         }
-        if(numberButton.getText().equals(".")){
+        if (numberButton.getText().equals(getString(R.string.decimal))) {
             numberHasDecimal = true;
         }
     }
@@ -82,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 currentMode = Mode.MULTIPLY;
                 break;
             default:
-                screen.setText("fail");
+                screen.setText(R.string.fail);
         }
         modeWasLast = true;
         savedNumber = Double.parseDouble(numberAsText);
@@ -98,11 +104,11 @@ public class MainActivity extends AppCompatActivity {
                 savedNumber = savedNumber - Double.parseDouble(numberAsText);
                 break;
             case DIVIDE:
-                if (numberAsText.equals("0")) {
-                    Toast toast = Toast.makeText(this, "Can't divide by Zero!", Toast.LENGTH_SHORT);
+                if (numberAsText.equals(getString(R.string.zero))) {
+                    Toast toast = Toast.makeText(this, R.string.cantDivideByZero, Toast.LENGTH_SHORT);
                     toast.show();
                 } else {
-                savedNumber = savedNumber / Double.parseDouble(numberAsText);
+                    savedNumber = savedNumber / Double.parseDouble(numberAsText);
                 }
                 break;
             case MULTIPLY:
@@ -114,13 +120,37 @@ public class MainActivity extends AppCompatActivity {
         numberAsText = nf.format(savedNumber);
         screen.setText(numberAsText);
         numberHasDecimal = false;
-
     }
 
-    public void onClickClear(View view){
+    public void onClickClear(View view) {
         savedNumber = 0;
-        numberAsText = "0";
+        numberAsText = getString(R.string.zero);
         screen.setText(numberAsText);
         numberHasDecimal = false;
+    }
+
+    public void onClickChangeBackground(View view) {
+
+        color = color + 1;
+
+        switch (color % 5) {
+            case 0:
+                background.setBackgroundColor(Color.WHITE);
+                break;
+            case 1:
+                background.setBackgroundColor(Color.BLUE);
+                screen.setTextColor(Color.RED);
+                break;
+            case 2:
+                background.setBackgroundColor(Color.RED);
+                screen.setTextColor(Color.BLACK);
+                break;
+            case 3:
+                background.setBackgroundColor(Color.YELLOW);
+                break;
+            case 4:
+                background.setBackgroundColor(Color.GREEN);
+                break;
+        }
     }
 }
